4c7142a3532a722a34c8af1c9254098e86d1f613
shirokovnv <shirokovnv@gmail.com>
2025-06-24 14:53:09 +0500

    Add bookings table

---
+++ app/Models/Booking.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\BookingFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Booking extends Model
+{
+    /** @use HasFactory<BookingFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'room_id',
+        'start_at',
+        'end_at',
+    ];
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
